1886c34b0ec947f69ff54033ecc03979f5a85415
schoopes <schoopes@gmail.com>
2019-01-16 19:08:55 -0800

    added gem, added ffaker actors

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@
 require 'ffaker'
 
 10.times do
-  actor = Actor.new(
+  Actor.create(
     first_name: FFaker::Name.first_name,
     last_name: FFaker::Name.last_name,
     known_for: FFaker::Movie.title
