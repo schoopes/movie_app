f9355aa3a16c9a2dd411b888b303b58f1df87742
schoopes <schoopes@gmail.com>
2019-01-20 09:30:18 -0800

    added params routes

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -3,4 +3,9 @@ class Api::ActorsController < ApplicationController
     @actor = Actor.first
     render 'one_actor.json.jbuilder'
   end
+
+  def one_actor_params
+    @actor_params = Actor.find_by(id: params["actor"])
+    render 'one_actor_query.json.jbuilder'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   namespace :api do
     get "/actors_url" => "actors#one_actor"
     get "/movies_url" => "movies#all_movies"
-    get "one_movie" => "movies#one_movie"
+    get "/one_movie" => "movies#one_movie"
+    get "/one_actor_query" => "actors#one_actor_params"
+    get "/one_actor_segment/:actor" => "actors#one_actor_params"
+    post "/one_actor_body" => "actors#one_actor_params"
   end
 end
